3d442006e348420005fef1255963fa06b96b8125
Remco <rpet@student.codam.nl>
2020-07-30 14:31:54 +0200

    Add files via upload

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rpet <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/27 12:49:31 by rpet          #+#    #+#                 */
-/*   Updated: 2020/07/27 14:03:55 by rpet          ########   odam.nl         */
+/*   Updated: 2020/07/30 14:07:27 by rpet          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,11 @@
 
 int		main(int argc, char **argv)
 {
-	int		i;
-
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		i = 1;
-		while (i < argc)
-		{
-			while (*argv[i])
-			{
-				std::cout << std::uppercase << *argv[i];
-				argv[i]++;
-			}
-			i++;
-		}
-		std::cout << std::endl;
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+	std::cout << std::endl;
 	return (0);
 }
