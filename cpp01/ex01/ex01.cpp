f58fdf015478913189df924ad32078eeb5e855c0
Remco <rpet@student.codam.nl>
2020-08-04 10:46:33 +0200

    Add files via upload

--- cpp01/ex01/ex01.cpp
+++ cpp01/ex01/ex01.cpp
@@ -1,1 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ex01.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rpet <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/04 09:21:25 by rpet          #+#    #+#                 */
+/*   Updated: 2020/08/04 09:22:37 by rpet          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <iostream>
+
+void	memoryLeak()
+{
+	std::string*	panther = new std::string("String panther");
+
+	std::cout << *panther << std::endl;
+	delete panther;
+}
