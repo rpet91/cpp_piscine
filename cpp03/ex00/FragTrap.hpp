cfb523d84b4178c34327521220f7ef34329a665e
Remco <rpet@student.codam.nl>
2021-03-22 16:22:40 +0100

    Add files via upload

---
+++ cpp03/ex00/FragTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   FragTrap.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rpet <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/22 08:11:08 by rpet          #+#    #+#                 */
+/*   Updated: 2021/03/22 13:16:00 by rpet          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+# include <iostream>
+
+class FragTrap {
+
+	public:
+		FragTrap(void);
+		FragTrap(std::string name);
+		~FragTrap(void);
+		FragTrap(FragTrap const &original);
+		FragTrap& operator=(FragTrap const &original);
+
+		void	rangedAttack(std::string const & target);
+		void	meleeAttack(std::string const & target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+		void			vaulthunter_dot_exe(std::string const & target);
+		std::string		getRndAtk(void);
+
+	private:
+
+		int				_hp;
+		int				_maxhp;
+		int				_ep;
+		int				_maxep;
+		int				_level;
+		std::string		_name;
+		int				_meleeAtk;
+		int				_rangedAtk;
+		int				_armorDmgRed;
+
+};
+
+#endif
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rpet <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/22 09:07:15 by rpet          #+#    #+#                 */
+/*   Updated: 2021/03/22 16:02:40 by rpet          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+#include <cstdlib>
+
+int		main(void)
+{
+	srand(time(NULL));
+
+	FragTrap	a;
+	FragTrap	b("Remco");
+
+	a.meleeAttack("The Impostor");
+	a.takeDamage(26);
+	a.rangedAttack("The Impostor");
+	a.beRepaired(15);
+	a.vaulthunter_dot_exe("Epic Spider");
+	a.vaulthunter_dot_exe("Epic Spider");
+	a.vaulthunter_dot_exe("Epic Spider");
+	a.vaulthunter_dot_exe("Epic Spider");
+	a.vaulthunter_dot_exe("Epic Spider");
+	return (0);
+}
