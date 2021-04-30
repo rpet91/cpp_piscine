c61bc0efa2a4604a6860181bd2da07fc8f8cdcb2
Remco <rpet@student.codam.nl>
2021-04-30 14:14:55 +0200

    Add files via upload

---
+++ cpp04/ex03/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rpet <marvin@codam.nl>                       +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/30 14:10:31 by rpet          #+#    #+#                 */
+/*   Updated: 2021/04/30 14:11:37 by rpet          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+#include "Character.hpp"
+#include "MateriaSource.hpp"
+#include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
+
+int	main()
+{
+	return (0);
+}
