d3b4278a50f902fe91efad77e4a51d0cbbbb8869
Remco <rpet@student.codam.nl>
2021-05-13 07:27:40 +0200

    Add files via upload

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,21 +6,26 @@
 /*   By: rpet <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/12 12:57:49 by rpet          #+#    #+#                 */
-/*   Updated: 2021/05/12 14:49:37 by rpet          ########   odam.nl         */
+/*   Updated: 2021/05/13 07:26:15 by rpet          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "serialization.hpp"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+
+struct	Data {
+	std::string	randomString1;
+	int			randomNumber;
+	std::string	randomString2;
+};
 
 std::string	createString()
 {
 	std::string	const	pool =
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	std::string			randomString;
-	int					randomLength = (rand() % 17) + 3;
+	int					randomLength = (rand() % 41) + 1;
 	
 	for (int i = 0; i < randomLength; i++)
 		randomString += pool[rand() % 62];
@@ -34,22 +39,25 @@ Data		*deserialize(void *raw)
 
 void		*serialize()
 {
-	Data				*serial = new Data();
+	Data	*data = new Data();
 
-	serial->randomString1 = createString();
-	serial->randomNumber = rand();
-	serial->randomString2 = createString();
-
-
-	std::cout << serial->randomString1 << std::endl;
-	std::cout << serial->randomNumber << std::endl;
-	std::cout << serial->randomString2 << std::endl;
-	return (serial);
+	data->randomString1 = createString();
+	data->randomNumber = rand();
+	data->randomString2 = createString();
+	return (reinterpret_cast<void *>(data));
 }
 
 int			main()
 {
 	srand(time(NULL));
+
 	void	*serial = serialize();
+	Data	*data = deserialize(serial);
+
+	std::cout << data->randomString1 << std::endl;
+	std::cout << data->randomNumber << std::endl;
+	std::cout << data->randomString2 << std::endl;
+
+	delete data;
 	return (0);
 }
