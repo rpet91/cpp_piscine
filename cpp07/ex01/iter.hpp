3df1ba0b6bdfd0d3cf41b8de2e7f38ed2c56a4a1
Remco <rpet@student.codam.nl>
2021-05-17 12:00:40 +0200

    Add files via upload

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: rpet <marvin@codam.nl>                       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/13 11:50:42 by rpet          #+#    #+#                 */
-/*   Updated: 2021/05/14 08:35:37 by rpet          ########   odam.nl         */
+/*   Updated: 2021/05/17 11:55:36 by rpet          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <cstddef>
 
 template <typename T>
-void	iter(T const *array, size_t const len, void (*func)(T element))
+void	iter(T const *array, size_t const len, void (*func)(T const &element))
 {
 	for (size_t i = 0; i < len; i++)
 		func(array[i]);
